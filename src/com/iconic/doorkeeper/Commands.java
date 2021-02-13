a455088083fa9eb1b00aff7dfbb589827d7b50c7
Phil <trekian00@googlemail.com>
2021-02-13 02:56:38 +0100

    Adding start of "create_team" method + "get_acctive_members" as an standalone method (increased modularity)

--- src/com/iconic/doorkeeper/Commands.java
+++ src/com/iconic/doorkeeper/Commands.java
@@ -1,16 +1,20 @@
 package com.iconic.doorkeeper;
 
-import net.dv8tion.jda.api.OnlineStatus;
-import net.dv8tion.jda.api.entities.GuildChannel;
-import net.dv8tion.jda.api.entities.Invite;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.VoiceChannel;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Commands extends ListenerAdapter {
+
+    private static Model model;
+
+    public Commands(){
+        model = new Model();
+    }
 
     // Distribute the command to the right Method
     public void onGuildMessageReceived(GuildMessageReceivedEvent event){
@@ -18,28 +22,42 @@ public class Commands extends ListenerAdapter {
 
         if(args[0].equalsIgnoreCase(Main.prefix+"show_members")){
             show_active_members(event);
+        }else if (args[0].equalsIgnoreCase(Main.prefix+"create_teams")){
+
+            if(args[1].equalsIgnoreCase("random")){
+                try{
+                    int amount_of_teams = Integer.parseInt(args[2]);
+                    create_teams(event,"random", null);
+
+                }catch (Exception e){
+                    event.getChannel().sendMessage("No valid number entered. Use '!create_teams ?' for more information")
+                            .queue();
+                }
+            }
+
+        }else if(args[0].equalsIgnoreCase(Main.prefix+"increase")){
+            model.increase_Just_test();
+            event.getChannel().sendMessage("just_test was "+ (model.getJust_test()-1) + " and is now" + model.getJust_test()  )
+                    .queue();
         }
     }
 
 
+    // Shows and count all User, who are in a VoiceChannel
     private void show_active_members(GuildMessageReceivedEvent event){
-        int current_amount = 0;
         StringBuilder online_members= new StringBuilder();
 
         List<VoiceChannel> voice_list = event.getGuild().getVoiceChannels();
+        List<Member> members_list = get_active_members(event);
+        int current_amount = members_list.size();
 
         System.out.println("Amount Voicechannels: "+voice_list.size());
 
-        for (VoiceChannel channel: voice_list) {
-
-            System.out.println("Name von Voicechannels: "+channel.getName());
-            int channel_size = channel.getMembers().size();
-            current_amount += channel_size;
-            for(int i = 0; i < channel_size;i++){
-                online_members.append(channel.getMembers().get(i).getUser().getName()).append(" ");
-            }
-
+        for (Member member: members_list) {
+            online_members.append(member.getUser().getName()).append(" ");
         }
+
+
         if(current_amount == 0){
             event.getChannel().sendMessage("Current amount of active Members: "+current_amount+ "\n" +
                     "Even YOU are not in a VoiceChannel :(")
@@ -49,7 +67,30 @@ public class Commands extends ListenerAdapter {
                     "Active Members: "+ online_members.toString())
                     .queue();
         }
+    }
 
+
+    private void create_teams(GuildMessageReceivedEvent event, String team, List<Member> member){
+
+    }
+
+    // Get all users who are CURRENTLY in a VoiceChannel.
+    private List<Member> get_active_members(GuildMessageReceivedEvent event){
+
+        List<VoiceChannel> voice_list = event.getGuild().getVoiceChannels();
+        List<Member> members_list = new ArrayList<Member>();
+
+        System.out.println("Amount Voicechannels: "+voice_list.size());
+
+        for (VoiceChannel channel: voice_list) {
+            System.out.println("Name von Voicechannels: "+channel.getName());
+            int channel_size = channel.getMembers().size();
+            for(int i = 0; i < channel_size;i++){
+                members_list.addAll(channel.getMembers());
+            }
+        }
+
+        return members_list;
 
     }
 }
---
+++ src/com/iconic/doorkeeper/Model.java
@@ -0,0 +1,29 @@
+package com.iconic.doorkeeper;
+
+/*
+This class is a kind of Database, to store data and to make a better overview and accessibility
+IMPORTANT: It stores data ONLY during runtime. As the running process is stopped, all data is gone!
+ */
+
+public class Model {
+
+    private int current_active_member;
+    private int amount_of_teams;
+    private int just_test;
+
+    public Model(){
+        just_test = 0;
+    }
+
+    public int getJust_test() {
+        return just_test;
+    }
+
+    public void setJust_test(int just_test) {
+        this.just_test = just_test;
+    }
+
+    public void increase_Just_test(){
+        just_test++;
+    }
+}
