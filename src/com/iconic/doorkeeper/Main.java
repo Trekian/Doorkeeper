475bae8b2d6c6547f1e246f3b678dddd9abcaf39
Phil <trekian00@googlemail.com>
2021-12-19 01:13:30 +0100

    Fixed: ReadFile for the bot token

--- src/com/iconic/doorkeeper/Main.java
+++ src/com/iconic/doorkeeper/Main.java
@@ -4,6 +4,8 @@ import com.iconic.doorkeeper.eventlistener.OnMessageReceived;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 
 import javax.security.auth.login.LoginException;
@@ -11,7 +13,10 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
     public static JDA jda;
@@ -20,7 +25,8 @@ public class Main {
     public static void main(String[] args) throws LoginException {
 
 
-        jda = JDABuilder.createDefault(get_token()).build();
+        String the_token = get_token();
+        jda = JDABuilder.createDefault(the_token).build();
         jda.getPresence().setActivity(Activity.watching("dir über die Schultern"));
 
         jda.addEventListener(new OnMessageReceived());
@@ -29,24 +35,20 @@ public class Main {
     private static String get_token(){
 
         try{
-            URL path = Main.class.getResource("bot_token.txt");
-            File file = new File(path.getFile());
-            BufferedReader br = new BufferedReader(new FileReader(file));
-            String line;
-            while ((line = br.readLine()) != null){
-                if (line.startsWith("token")){
-                    int start_index = line.indexOf('=')+1;
-                    return line.substring(start_index);
-                }
-            }
+
+            Path filename = Path.of("src/com/iconic/doorkeeper/bot_token.txt");
+            File file = filename.toFile();
+            Scanner sc = new Scanner(file);
+            //Skipping first line as it is only a comment
+            sc.nextLine();
+            return sc.nextLine();
+
 
         }catch (Exception e){
             System.out.println("Something is wrong with the Token. Maybe expired, or a new one was generated");
             System.out.println(Arrays.toString(e.getStackTrace()));
             return null;
         }
-        return null;
-
     }
 
 }
